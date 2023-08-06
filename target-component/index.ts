106bb8ce5c451d8460836d6f8ce65fd21682e82b
Thomas Riffard <ferdodo@hotmail.fr>
2023-08-06 12:48:31 +0200

    click on target to commit candidate

--- target-component/index.ts
+++ target-component/index.ts
@@ -4,6 +4,7 @@ import { Target } from "../target";
 import { getTarget } from "../get-target";
 import { observeTarget } from "../observe-target";
 import { blockXEnd, blockYEnd } from "../block";
+import { commitCandidate } from "../puzzle-state";
 
 export const TargetComponent = defineComponent({
     props: {
@@ -15,7 +16,7 @@ export const TargetComponent = defineComponent({
 	setup(props) {
 		const target: Ref<Target> = ref(getTarget(props.id));
 		observeTarget(props.id).subscribe(value => target.value = value);
-		return { target, blockXEnd, blockYEnd };
+		return { target, commitCandidate, blockXEnd, blockYEnd };
 	},
 	render
 });
