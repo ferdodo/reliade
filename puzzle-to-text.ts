350230af4c99d6efe235cd1d742c04d9dd37a31f
Thomas Riffard <ferdodo@hotmail.fr>
2025-03-31 09:27:01 +0200

    Fix puzzle to text axis

--- puzzle-to-text.ts
+++ puzzle-to-text.ts
@@ -4,8 +4,8 @@ import { puzzleToSymbol } from "./puzzle-to-symbol";
 export function puzzleToText(puzzle: Puzzle): string {
 	let result = "";
 
-	for (let x = 0; x < puzzle.block.w; x++) {
-		for (let y = 0; y < puzzle.block.h; y++) {
+	for (let y = 0; y < puzzle.block.h; y++) {
+		for (let x = 0; x < puzzle.block.w; x++) {
 			result += puzzleToSymbol(puzzle, x, y);
 		}
 
